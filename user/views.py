3c1e038c24a863e08b0c3eb745e9afc876e1a767
leechannie <ioio000213@gmail.com>
2022-11-27 11:22:58 +0900

    회원가입 후, 카테고리 바로 이동 추가

--- user/views.py
+++ user/views.py
@@ -71,7 +71,7 @@ def user_signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)  # 사용자 인증
             login(request, user)  # 로그인
-            return redirect('index')
+            return render(request,'user/catecory.html')
     else:
         form = UserForm()
         print(form.GENDER_CHOICES)
