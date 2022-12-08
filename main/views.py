6ff456650f57b4c5663f6b1f8870e947dd683f9f
Choi mieun <104505272+chi980@users.noreply.github.com>
2022-12-08 23:33:06 +0900

    1208

--- main/views.py
+++ main/views.py
@@ -17,110 +17,139 @@ from decimal import Decimal
 def index(request):
     recommended = None
     locations_pet = None
-    if request.user.is_authenticated:
-            #남서쪽 위도 경도(swLatlng): '33.44953470033243, 126.56849976573322'
-            #북동쪽 위도 경도(neLatlng): '33.451124628187245, 126.5719981820737'
-
-            cur_longitude = 127.0117172
-            cur_latitude = 37.5563644
-            cur_scale = 50.0
-            cur_longitude_min = cur_longitude - cur_scale
-            cur_latitude_min = cur_latitude - cur_scale
-            cur_longitude_max = cur_longitude + cur_scale
-            cur_latitude_max = cur_latitude + cur_scale
-            print(cur_latitude_min,cur_latitude_max)
-            print( cur_longitude_min, cur_longitude_max)
-
-            # cur_latitude_min = str(request.swLatlng).split(,)[0]
-            # cur_latitude_max = str(request.neLatlng).split(,)[0]
-            # cur_longitude_min = str(request.swLatlng).split(,)[1]
-            # cur_longitude_max = str(request.neLatlng).split(,)[1]
-
-            # locations = Location.objects.all()
-
-            # recommended = Review.objects.filter(Q(location_id__in=locations) & Q(category=request.user.category)).values('location').annotate(avg=Avg('star')).order_by('-avg')[:10]
-
-            # locations = Location.objects.filter(Q(logitude__range=[cur_longitude_min,cur_longitude_max]) & Q(latitude__range=[cur_latitude_min,cur_latitude_max])).values_list('id',flat=True)
-            # locations = Location.objects.filter(Q(logitude__range=[cur_longitude_min,cur_longitude_max]) & Q(latitude__range=[cur_latitude_min,cur_latitude_max]))
-            locations = Location.objects.filter(Q(logitude__range=[cur_longitude_min,cur_longitude_max]) & Q(latitude__range=[cur_latitude_min,cur_latitude_max]))
-            locations_res = locations.filter(Q(on_off = 0)).values_list('id',flat=True)
-
-            if(request.user.on_off == 1):
-                locations_pet = locations.filter(Q(on_off = 1))
-            print(locations_pet)
-            recommended= Review.objects.filter(Q(location_id__in=locations_res) & Q(category=request.user.category)).values('location','location__name','location__address','location__lot_address','location__phone','location__time','location__url','location__is_animal_in','location__latitude','location__logitude').annotate(avg=Avg('star')).order_by('-avg')[:10]
-
-    return render(request,'main/index.html',{'recommended':recommended,'locations_pet':locations_pet})
-
-@csrf_exempt
-@require_POST
-def recommended(request):
-    jsonObject = json.loads(request.body)
-    # print("Ajax 데이터를 받았음")
-    # print("남서쪽:",jsonObject.get('swLatlng'))
-    # print("북동쪽:",jsonObject.get('neLatlng'))
-    cur_latitude_min = jsonObject.get('swLatlng')['Ma']
-    cur_latitude_max = jsonObject.get('neLatlng')['Ma']
-    cur_longitude_min = jsonObject.get('swLatlng')['La']
-    cur_longitude_max = jsonObject.get('neLatlng')['La']
-
-    # cur_latitude_min = 37.47919116788586
-    # cur_latitude_max = 37.48531880573196
-    # cur_longitude_min = 126.97144148512074
-    # cur_longitude_max = 126.97486509760063
-
-    locations_pet = None
-    recommended = None
     locations_pet_list = None
     recommended_list = None
-    if request.user.is_authenticated:
-        # 해당 지역 내의 location
-        print(cur_longitude_min,cur_longitude_max)
-        print(cur_latitude_min,cur_latitude_max)
-        locations = Location.objects.filter(Q(logitude__range=(cur_longitude_min, cur_longitude_max)) & Q(latitude__range=(cur_latitude_min, cur_latitude_max)))
-        # locations = Location.objects.all()
-        print("*"*200)
-        print(locations)
-        locations_res = locations.filter(Q(on_off=0)).values_list('id', flat=True)
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            cur_latitude_min = request.get('swLatlng_lat')
+            cur_latitude_max = request.get('neLatlng_lat')
+            cur_longitude_min = request.get('swLatlng_lon')
+            cur_longitude_max = request.get('neLatlng_lon')
 
-        if (request.user.on_off == 1):
-            locations_pet = locations.filter(Q(on_off=1))
-            locations_pet_list = serializers.serialize('json', locations_pet)
-        if (request.user.category):
-            print("user는 category가지고 있음")
+            locations = Location.objects.filter(Q(logitude__range=(cur_longitude_min, cur_longitude_max)) & Q(latitude__range=(cur_latitude_min, cur_latitude_max)))
+            locations_res = locations.filter(Q(on_off=0)).values_list('id', flat=True)
 
-            recommended = Review.objects.filter(Q(category_id=request.user.category)&Q(location_id__in=locations_res)).values('location_id').annotate(star_avg=Avg('star'))[:5]\
-                .values('location_id', 'location__name', 'location__category',
-                                     'location__address','location__lot_address','location__phone',
-                                     'location__time','location__url','location__is_animal_in',
-                                     'location__latitude','location__logitude','location__on_off','star_avg')
-            # print(recommended)
-            recommended_list = json.dumps(list(recommended),cls=DecimalEncoder, ensure_ascii=False)
-            print(recommended_list)
+            # pet관련 장소 리스트
+            if (request.user.on_off == 1):
+                locations_pet = locations.filter(Q(on_off=1))
+                locations_pet_list = serializers.serialize('json', locations_pet)
+
+            # 추천 리스트
+            if (request.user.category):
+                recommended = Review.objects.filter(
+                    Q(category_id=request.user.category) & Q(location_id__in=locations_res)).values('location_id').annotate(
+                    star_avg=Avg('star'))[:5] \
+                    .values('location_id', 'location__name', 'location__category',
+                            'location__address', 'location__lot_address', 'location__phone',
+                            'location__time', 'location__url', 'location__is_animal_in',
+                            'location__latitude', 'location__logitude', 'location__on_off', 'star_avg')
+                # print(recommended)
+                recommended_list = json.dumps(list(recommended), cls=DecimalEncoder, ensure_ascii=False)
+            else:
+                # response = JsonResponse({"error": "카테고리 설정이 필요합니다."})
+                # response.status_code = 403  # To announce that the user isn't allowed to publish
+                # return response
+                return HttpResponse("<script>alert('카테고리 설정이 필요합니다.');location.href='/';</script>")
+            if not recommended:
+                # response = JsonResponse({"error": "추천할 장소가 없습니다."})
+                # response.status_code = 403  # To announce that the user isn't allowed to publish
+                # return response
+                return HttpResponse("<script>alert('추천할 장소가 없습니다.');location.href='/';</script>")
         else:
-            response = JsonResponse({"error": "카테고리 설정이 필요합니다."})
-            response.status_code = 403  # To announce that the user isn't allowed to publish
-            return response
-        # if (recommended):
-        #     response = JsonResponse({"error": "추천할 장소가 없습니다."})
-        #     response.status_code = 403  # To announce that the user isn't allowed to publish
-        #     return response
-        # return HttpResponse({locations_pet_list,recommended_list}, content_type="text/json-comment-filtered")
-        # return HttpResponse({"locations_pet_list":locations_pet_list,"recommended_list":recommended_list}, content_type="text/json-comment-filtered")
-        return JsonResponse({"recommended":recommended_list,"locations_pet":locations_pet_list}, safe=False)
-    # Category 일단 보내보기
+            # response = JsonResponse({"error": "로그인이 필요합니다."})
+            # response.status_code = 403  # To announce that the user isn't allowed to publish
+            # return response
+            return HttpResponse("<script>alert('로그인이 필요합니다.');location.href='/';</script>")
+        return render(request,'main/index',{"recommended":recommended,"location_pet":locations_pet,"locations":locations})
     # if request.user.is_authenticated:
-    #     if not request.user.category:
-    #         response = JsonResponse({"error": "카테고리 설정이 필요합니다."})
-    #         response.status_code = 403  # To announce that the user isn't allowed to publish
-    #         return response
-    #     categories = Category.objects.all()
-    #     category_list = serializers.serialize('json',categories)
-    #     return HttpResponse(category_list,content_type="text/json-comment-filtered")
+    #         #남서쪽 위도 경도(swLatlng): '33.44953470033243, 126.56849976573322'
+    #         #북동쪽 위도 경도(neLatlng): '33.451124628187245, 126.5719981820737'
+    #
+    #         cur_longitude = 127.0117172
+    #         cur_latitude = 37.5563644
+    #         cur_scale = 50.0
+    #         cur_longitude_min = cur_longitude - cur_scale
+    #         cur_latitude_min = cur_latitude - cur_scale
+    #         cur_longitude_max = cur_longitude + cur_scale
+    #         cur_latitude_max = cur_latitude + cur_scale
+    #         print(cur_latitude_min,cur_latitude_max)
+    #         print( cur_longitude_min, cur_longitude_max)
+    #
+    #         # cur_latitude_min = str(request.swLatlng).split(,)[0]
+    #         # cur_latitude_max = str(request.neLatlng).split(,)[0]
+    #         # cur_longitude_min = str(request.swLatlng).split(,)[1]
+    #         # cur_longitude_max = str(request.neLatlng).split(,)[1]
+    #
+    #         # locations = Location.objects.all()
+    #
+    #         # recommended = Review.objects.filter(Q(location_id__in=locations) & Q(category=request.user.category)).values('location').annotate(avg=Avg('star')).order_by('-avg')[:10]
+    #
+    #         # locations = Location.objects.filter(Q(logitude__range=[cur_longitude_min,cur_longitude_max]) & Q(latitude__range=[cur_latitude_min,cur_latitude_max])).values_list('id',flat=True)
+    #         # locations = Location.objects.filter(Q(logitude__range=[cur_longitude_min,cur_longitude_max]) & Q(latitude__range=[cur_latitude_min,cur_latitude_max]))
+    #         locations = Location.objects.filter(Q(logitude__range=[cur_longitude_min,cur_longitude_max]) & Q(latitude__range=[cur_latitude_min,cur_latitude_max]))
+    #         locations_res = locations.filter(Q(on_off = 0)).values_list('id',flat=True)
+    #
+    #         if(request.user.on_off == 1):
+    #             locations_pet = locations.filter(Q(on_off = 1))
+    #         print(locations_pet)
+    #         recommended= Review.objects.filter(Q(location_id__in=locations_res) & Q(category=request.user.category)).values('location','location__name','location__address','location__lot_address','location__phone','location__time','location__url','location__is_animal_in','location__latitude','location__logitude').annotate(avg=Avg('star')).order_by('-avg')[:10]
+    #
+    # return render(request,'main/index.html')
     else:
-        response = JsonResponse({"error": "로그인이 필요합니다."})
-        response.status_code = 403  # To announce that the user isn't allowed to publish
-        return response
+        return render(request,'main/index.html')
+# @csrf_exempt
+# @require_POST
+# def recommended(request):
+#     jsonObject = json.loads(request.body)
+#     # print("Ajax 데이터를 받았음")
+#     # print("남서쪽:",jsonObject.get('swLatlng'))
+#     # print("북동쪽:",jsonObject.get('neLatlng'))
+#     cur_latitude_min = jsonObject.get('swLatlng')['Ma']
+#     cur_latitude_max = jsonObject.get('neLatlng')['Ma']
+#     cur_longitude_min = jsonObject.get('swLatlng')['La']
+#     cur_longitude_max = jsonObject.get('neLatlng')['La']
+#
+#     # cur_latitude_min = 37.47919116788586
+#     # cur_latitude_max = 37.48531880573196
+#     # cur_longitude_min = 126.97144148512074
+#     # cur_longitude_max = 126.97486509760063
+#
+#     locations_pet = None
+#     recommended = None
+#     locations_pet_list = None
+#     recommended_list = None
+#     if request.user.is_authenticated:
+#         # 해당 지역 내의 location
+#         locations = Location.objects.filter(Q(logitude__range=(cur_longitude_min, cur_longitude_max)) & Q(latitude__range=(cur_latitude_min, cur_latitude_max)))
+#         locations_res = locations.filter(Q(on_off=0)).values_list('id', flat=True)
+#
+#         # pet관련 장소 리스트
+#         if (request.user.on_off == 1):
+#             locations_pet = locations.filter(Q(on_off=1))
+#             locations_pet_list = serializers.serialize('json', locations_pet)
+#
+#         # 추천 리스트
+#         if (request.user.category):
+#             recommended = Review.objects.filter(Q(category_id=request.user.category)&Q(location_id__in=locations_res)).values('location_id').annotate(star_avg=Avg('star'))[:5]\
+#                 .values('location_id', 'location__name', 'location__category',
+#                                      'location__address','location__lot_address','location__phone',
+#                                      'location__time','location__url','location__is_animal_in',
+#                                      'location__latitude','location__logitude','location__on_off','star_avg')
+#             # print(recommended)
+#             recommended_list = json.dumps(list(recommended),cls=DecimalEncoder, ensure_ascii=False)
+#         else:
+#             response = JsonResponse({"error": "카테고리 설정이 필요합니다."})
+#             response.status_code = 403  # To announce that the user isn't allowed to publish
+#             return response
+#         if not recommended:
+#             response = JsonResponse({"error": "추천할 장소가 없습니다."})
+#             response.status_code = 403  # To announce that the user isn't allowed to publish
+#             return response
+#         return JsonResponse({"recommended":recommended_list,"locations_pet":locations_pet_list}, safe=False)
+#     else:
+#         response = JsonResponse({"error": "로그인이 필요합니다."})
+#         response.status_code = 403  # To announce that the user isn't allowed to publish
+#         return response
 
 
 class DecimalEncoder(json.JSONEncoder):
@@ -132,3 +161,5 @@ class DecimalEncoder(json.JSONEncoder):
             return str(round(obj,2))
         # 👇️ otherwise use the default behavior
         return json.JSONEncoder.default(self, obj)
+
+
--- static/map.js
+++ static/map.js
@@ -206,55 +206,50 @@ function getCurrentPosBtn() {
 //    console.log('영역이 변경됩니다.');
 //    myMapInfo();
 //});
-
-kakao.maps.event.addListener(map, "idle", function () {
-  console.log("idle상태입니다.");
-  // 지도 영역정보를 얻어옵니다
-  var bounds = map.getBounds();
-
-  // 영역정보의 남서쪽 정보를 얻어옵니다
-  var swLatlng = bounds.getSouthWest();
-
-  // 영역정보의 북동쪽 정보를 얻어옵니다
-  var neLatlng = bounds.getNorthEast();
-  var mapinfo = {
-    swLatlng: swLatlng,
-    neLatlng: neLatlng,
-  };
-  var message =
-    "<p>영역좌표는 남서쪽 위도, 경도는  " + swLatlng.toString() + "이고 <br>";
-  message += "북동쪽 위도, 경도는  " + neLatlng.toString() + "입니다 </p>";
-  console.log(message);
-  $.ajax({
-    url: "/main/recommended/",
-    type: "POST",
-    headers: {
-      "X-CSRFTOKEN": "{{ csrf_token }}",
-    },
-    data: JSON.stringify(mapinfo),
-    success: function (data) {
-      // AJAX 통신 성공시 받은 데이터를 console에 print
-      console.log(data);
-      // 만약 login되어 있고 category선택한 경우, json 리스트 형태로 데이터 받아서 첫 번쨰 요소 console에 출력하고, 임시로 만든 p태그에 첫 번째 요소 set
-      //            if(Array.isArray(data)){
-      //                console.log(data[0])
-      //                console.log(data[0]['fields'].name)
-      //                console.log(data[0]['fields'].contents)
-      //                console.log(data[0]['fields'].profile)
-      //                const del_p= document.getElementById('delete_p');
-      //                del_p.innerHTML = data[0]['fields'].name + data[0]['fields'].contents + data[0]['fields'].profile
-      //            }
-    },
-    error: function (data) {
-      // AJAX 통신 실패시 alert창
-      //            alert(data.status); // the status code
-      console.log(data);
-      if (data.responseJSON.error) {
-        alert(data.responseJSON.error);
-      }
-    },
-  });
-});
+/*
+//kakao.maps.event.addListener(map, "idle", function () {
+//  console.log("idle상태입니다.");
+//  // 지도 영역정보를 얻어옵니다
+//  var bounds = map.getBounds();
+//
+//  // 영역정보의 남서쪽 정보를 얻어옵니다
+//  var swLatlng = bounds.getSouthWest();
+//
+//  // 영역정보의 북동쪽 정보를 얻어옵니다
+//  var neLatlng = bounds.getNorthEast();
+//  var mapinfo = {
+//    swLatlng: swLatlng,
+//    neLatlng: neLatlng,
+//  };
+//  var message =
+//    "<p>영역좌표는 남서쪽 위도, 경도는  " + swLatlng.toString() + "이고 <br>";
+//  message += "북동쪽 위도, 경도는  " + neLatlng.toString() + "입니다 </p>";
+//  console.log(message);
+//  $.ajax({
+//    url: "/main/recommended/",
+//    type: "POST",
+//    headers: {
+//      "X-CSRFTOKEN": "{{ csrf_token }}",
+//    },
+//    data: JSON.stringify(mapinfo),
+//    success: function (data) {
+//      // AJAX 통신 성공시 받은 데이터를 console에 print
+//      console.log("성공~")
+//      console.log(data);
+//      del_p = document.getElementById("delete_p");
+//      del_p.innerHTML = data.recommended;
+//    },
+//    error: function (data) {
+//      // AJAX 통신 실패시 alert창
+//      //            alert(data.status); // the status code
+//      console.log(data);
+//      if (data.responseJSON.error) {
+//        alert(data.responseJSON.error);
+//      }
+//    },
+//  });
+//});
+*/
 //function myMapInfo(){
 //    // 지도 영역정보를 얻어옵니다
 //    var bounds = map.getBounds();
